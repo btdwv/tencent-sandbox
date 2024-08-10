91697b2bb5790aa659539b2e588ebe6a1b705061
btdwv <btdwv@163.com>
2024-08-10 14:49:54 +0800

    增加夸克网盘、QQ音乐、网易云音乐的目录配置
    1. 增加夸克网盘、QQ音乐、网易云音乐的目录配置
    2. 上传一些腾讯全家桶可能需要的32位库

--- Scripts/sandbox-setup.cmd
+++ Scripts/sandbox-setup.cmd
@@ -1,3 +1,8 @@
+::通过这个脚本，将简体中文（zh-Hans-CN）添加到当前用户的语言列表
 powershell.exe -executionpolicy bypass -file C:\Scripts\addzh-cn.ps1
 
+::安装腾讯TIM时，它会在SysWOW64添加一些dll文件，通过这个命令，把这些文件复制到SysWOW64里
+xcopy "C:\SysWOW64\*" "C:\Windows\SysWOW64\" /E /H /C /I /Y
+
+::运行QQ的守护程序
 C:\Program Files (x86)\Common Files\Tencent\QQProtect\Bin\QQProtect.exe
--- mkdir.bat
+++ mkdir.bat
@@ -1,4 +1,19 @@
 @echo off
+
+::其他
+md "App"
+md "Desktop"
+md "Scripts"
+md "Data"
+md "Data\SysWOW64"
+md "Data\Common Files"
+md "Data\Common Files (x86)"
+md "Data\Local"
+md "Data\Roaming"
+md "Data\Documents"
+md "Data\ProgramData"
+
+::腾讯全家桶
 md "App\QQ"
 md "App\QQNT"
 md "App\TIM"
@@ -6,15 +21,44 @@ md "App\WeChat"
 md "App\WXWork"
 md "App\Meeting"
 md "App\TencentDocs"
-md "Data\Common Files"
+md "App\QQMusic"
 md "Data\Documents\Tencent"
 md "Data\Documents\WeChat"
 md "Data\Documents\WXWork"
+md "Data\Documents\QQMusic"
 md "Data\Roaming\Tencent"
 md "Data\Roaming\WeChat"
 md "Data\Roaming\TencentDocs"
 md "Data\Roaming\WeMeet"
-md "Data\SysWOW64"
+md "Data\Roaming\QQMusic"
 md "Data\ProgramData\Tencent"
-md "Desktop"
-md "Scripts"
+
+::网易云音乐
+md "App\CloudMusic"
+md "Data\Local\CloudMusic"
+md "Data\Documents\CloudMusic"
+
+::ToDesk
+::md "App\ToDesk"
+::md "Data\Local\ToDesk"
+
+::向日葵
+::md "App\SunLogin"
+::md "Data\Roaming\SunloginClient"
+::md "Data\ProgramData\Oray"
+::md "Data\ProgramData\OrayClient"
+
+::KOOK
+md "App\KOOK"
+md "Data\Roaming\KOOK"
+md "Data\Documents\KOOK"
+
+::百度网盘
+md "Data\Roaming\baidu"
+md "Data\Roaming\BaiduYunGuanjia"
+md "Data\Roaming\BaiduYunKernel"
+md "Data\Roaming\Adobe"
+
+::夸克网盘
+md "App\quark-cloud-drive"
+md "Data\Roaming\quark-cloud-drive"
